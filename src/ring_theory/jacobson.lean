7c33d9b57cc2f969e15dc1c8765a7e9171d8a333
Devon Tuma <tumax040@umn.edu>
2020-07-23 19:09:54 -0500

    feat(ring_theory/jacobson): create scafolding for proof of is_jacobson polynomial

--- src/ring_theory/jacobson.lean
+++ src/ring_theory/jacobson.lean
@@ -113,96 +113,43 @@ section polynomial
 variables {R : Type u} [comm_ring R] {I : ideal R}
 open polynomial
 
-noncomputable theory
-
-lemma coeff_zero_mem_of_mem_span_C {f : polynomial R} :
-  f ∈ span (C '' (I : set R)) → coeff f 0 ∈ I :=
+lemma help (P : ideal (polynomial R)) (hP : is_prime P) :
+  is_integral_domain R → ∃ M ⊆ {J : ideal (polynomial R) | J.is_maximal ∨ J = ⊤}, P = Inf M :=
 begin
-  intro h,
-  apply submodule.span_induction h,
-  { intros g hg,
-    rw set.mem_image at hg,
-    rcases hg with ⟨x, ⟨hx, hxg⟩⟩,
-    rwa [← hxg, coeff_C_zero] },
-  { exact I.zero_mem },
-  { intros g g' hg hg',
-    simp,
-    exact I.add_mem hg hg' },
-  { intros x g hg,
-    simp,
-    exact I.smul_mem _ hg }
-end
-
-theorem polynomial_exists_downshift (f : polynomial R) : ∃ g, f = g * X + C (coeff f 0) :=
-begin
-  use f.sum (λn a, if n > 0 then C a * X^(n - 1) else 0),
-  ext,
-  cases n,
-  { simp },
-  { rw [coeff_add, coeff_C_succ, add_zero, coeff_mul_X],
-    simp only [coeff_X_pow, coeff_sum, coeff_C_mul],
-    rw [finsupp.sum, finset.sum_eq_single (n + 1)],
-    { simp,
-      exact congr_fun rfl n.succ },
-    { intros m hm h,
-      cases m,
-      { exact trans (by refl) (coeff_zero n) },
-      { have : n ≠ m := λ h', h (by rw h'),
-        simp [this, coeff_X_pow] } },
-    { simp_intros hn,
-      exact hn } }
-end
-
-/-- `restrict_const_coeff I` is the ideal of polynomials with constant coefficient in I -/
-def restrict_const_coeff (I : ideal R) : ideal (polynomial R) := span (insert X (C '' (I : set R)))
-
-lemma mem_restrict_const_coeff_iff_coeff_zero_mem {f : polynomial R} :
-  f ∈ restrict_const_coeff I ↔ coeff f 0 ∈ I :=
-begin
-  split,
-  { intro h,
-    rcases mem_span_insert.1 h with ⟨f', g, hg, hf⟩,
-    simp [hf, coeff_zero_mem_of_mem_span_C hg] },
-  { cases polynomial_exists_downshift f with g hg,
-    exact λ h, mem_span_insert.2 ⟨g, C (f.coeff 0), mem_map_of_mem h, hg⟩ }
-end
-
-lemma restrict_const_coeff.is_prime [H : is_prime I] : is_prime (restrict_const_coeff I) :=
-begin
-  split,
-  { refine λ h, H.left _,
-    rw eq_top_iff,
-    intros x _,
-    have : C x ∈ restrict_const_coeff I := h.symm ▸ submodule.mem_top,
-    rwa [mem_restrict_const_coeff_iff_coeff_zero_mem, coeff_C] at this },
-  { intros f g hfg,
-    rw mem_restrict_const_coeff_iff_coeff_zero_mem at hfg,
-    simp at hfg,
-    cases H.right hfg,
-    { left,
-      rwa mem_restrict_const_coeff_iff_coeff_zero_mem },
-    { right,
-      rwa mem_restrict_const_coeff_iff_coeff_zero_mem } }
+  sorry
 end
 
 theorem is_jacobson_polynomial_iff_is_jacobson : is_jacobson R ↔ is_jacobson (polynomial R) :=
 begin
-  split; intro H,
+  split; introI H,
   {
-    sorry,
+    rw is_jacobson_iff_Inf_maximal,
+    introsI P hP,
+    let P₀ : ideal R := comap C P,
+    have hP₀ : is_prime P₀ := sorry,
+    let Q : ideal (polynomial R) := map C P₀,
+    let R' := ideal.quotient P₀,
+    have e : (ideal.quotient Q) ≃+* polynomial R' := sorry,
+    let ϕ : polynomial R →+* polynomial R' := e.to_ring_hom.comp (quotient.mk Q),
+    let P' : ideal (polynomial R') := map ↑e (map (quotient.mk Q) P),
+    have hR' : is_integral_domain R' := sorry,
+    have hR'_jac : is_jacobson R' := by apply_instance,
+    have hP' : is_prime P' := sorry,
+    rcases help P' hP' hR' with ⟨M, hM, hMP'⟩,
+    let M' : set (ideal (polynomial R)) := (λ J, comap ϕ J) '' M,
+    use M',
+    split,
+    {
+      sorry,
+    },
+    {
+      have : P = comap ϕ P' := sorry,
+      rw [this, hMP'],
+      rw Inf_image,
+      rw comap_Inf,
+    }
   },
-  { rw is_jacobson_iff_prime_eq,
-    introsI P₀ hP₀,
-    refine le_antisymm _ le_jacobson,
-    intros x hx,
-    suffices : coeff (C x) 0 ∈ P₀, by rwa coeff_C_zero at this,
-    rw [← mem_restrict_const_coeff_iff_coeff_zero_mem,
-      ← H (restrict_const_coeff P₀) (is_prime.radical restrict_const_coeff.is_prime)],
-    rw mem_jacobson_iff at ⊢ hx,
-    intros f,
-    cases hx (coeff f 0) with y hy,
-    use C y,
-    simp [mem_restrict_const_coeff_iff_coeff_zero_mem, hy] }
+  { exact is_jacobson_of_surjective ⟨eval₂_ring_hom (ring_hom.id _) 1, λ x, ⟨C x, by simp⟩⟩ }
 end
 
 end polynomial
